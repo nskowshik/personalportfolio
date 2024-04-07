dc700ac37f292f847001403b2445a8a449f7753c
Kowshik Sudarsan <kowshik.sudarsan@superops.ai>
2024-04-07 12:10:56 +0530

    Removed styled component

--- src/pages/Careers.js
+++ src/pages/Careers.js
@@ -1,27 +1,7 @@
 import React from 'react';
 import { WORK_EXPERIANCE } from '../constants'
 import { TextStyle } from '../components'
-import styled from 'styled-components'
 
-const CardWrapper = styled.div`
-  background: #41424157;
-  height: 250px;
-  margin: 10px;
-  border-radius: 10px;
-  padding: 20px 40px;
-  &:hover{
-    box-shadow: #7979791f 8px 8px;
-    transition: transform 0.3s;
-    transform: translateY(-10px);
-    .companyName{
-      text-shadow: 10px 15px 2px #ffffff42;
-      opacity: 0.75;
-    }
-  }
-  .companyName{
-    font-size: 24px;
-  }
-`
 const Careers = () => {
   return(
     <div className="mx40 my20">
@@ -29,13 +9,13 @@ const Careers = () => {
       <div className="fx jctr">
         {WORK_EXPERIANCE.map(el => {
           return(
-            <CardWrapper className="fx2">
+            <div className="fx2">
               <div className="fx">
                 <div className="fx2 actr companyName">{el.companyName}</div>
                 <img src={el?.logo?.url} width={el?.logo?.width} height={el?.logo?.height} />
               </div>
               <p>{el.startDate} - {el.endDate || el.isPresent && "Present"}</p>
-            </CardWrapper>
+            </div>
           )
         })}
       </div>
--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import { TextStyle } from '../components'
 import { PROFILE_INFO } from '../constants'
-import styled from 'styled-components';
-
-const IntroWrapper = styled.div`
-  min-height: 500px;
-  padding: 0rem 8rem;
-`
 
 const Intro = () => {
   return(
-    <IntroWrapper className="row">
+    <div className="row">
       <div className="col-lg-6 col-sm-12 jctr actr">
         <div>
           <TextStyle size="26">Hello, it's me </TextStyle>
@@ -21,7 +15,7 @@ const Intro = () => {
       <div className="col-lg-6 col-sm-12 jctr actr">
         <img width="350px" height="300px" type="image/jpeg" src="https://drive.google.com/uc?export=view&id=1CyTS_MuBDsx4LV5sT-6OO3B-85yEP_1W"></img>
       </div>
-    </IntroWrapper>
+    </div>
   )
 }
 
