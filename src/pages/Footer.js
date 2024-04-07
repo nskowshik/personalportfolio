7c6625838d5d3e1b24ee8e55a9ba217a254b72d6
Kowshik Sudarsan <kowshik.sudarsan@superops.ai>
2024-04-07 12:06:48 +0530

    Removed unwanted files

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {SVGIcon} from '../components';
 import { ReactSVG } from 'react-svg'
-import styled from 'styled-components';
 
 const Footer = () => {
   const media= [
