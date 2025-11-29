b21a1dc39051fb7c426423c6337a3da0baf02aa5
Kowshik Sudarsan <kowshik.sudarsan@superops.ai>
2025-11-29 13:54:07 +0530

    Toolbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import Intro from './Intro'
 import Footer from './Footer'
 import Careers from './Careers'
+import Toolbar from './Toolbar'
 export {
   Intro,
   Careers,
+  Toolbar,
   Footer
 }
