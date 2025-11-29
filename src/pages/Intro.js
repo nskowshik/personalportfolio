0363db8533115fa754eca84e3512aa6f01f2004c
Kowshik Sudarsan <kowshik.sudarsan@superops.ai>
2025-11-29 16:56:45 +0530

    Image

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -5,11 +5,17 @@ import { PROFILE_INFO } from "../constants";
 const Intro = () => {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center">
-        <TextStyle size="26">Hello, it's me </TextStyle>
-        <TextStyle size="42">{PROFILE_INFO.name}</TextStyle>
-        <TextStyle size="18" color="#d0d0d0eb" className="underline">
-          {PROFILE_INFO.about}
-        </TextStyle>
+      <img
+        src="/image/kowshik_2025.png"
+        alt="Kowshik Sudarsan"
+        width={250}
+        height={250}
+        className="rounded-lg mb-4"
+      />
+      <TextStyle size="42">{PROFILE_INFO.name}</TextStyle>
+      <TextStyle size="18" color="#d0d0d0eb" className="underline">
+        {PROFILE_INFO.about}
+      </TextStyle>
     </div>
   );
 };
